2624e31c1faaa15b2900e6e33d48eb64b5bece61
paula <paularelt@gmail.com>
2020-05-13 07:53:42 -0400

    Full product

--- gen.py
+++ gen.py
@@ -4,7 +4,24 @@ specials = ['!','@','#','$','%','^','&','*']
 caps = [chr(i) for i in range(65,91)]
 lowers = [i.lower() for i in caps]
 
-def main(len):
+def main(mlen):
+	global specials
+	global caps
+	global lowers
+	
 	xpass = ""
-	for i in range(len):
-		xpass += chr(random)
+	for i in range(mlen):
+		rc = int(random.randrange(0,3))
+		if rc == 0:
+			xpass += specials[random.randrange(0, len(specials))]
+		elif rc == 1:
+			xpass += caps[random.randrange(0, len(caps))]
+		else:
+			xpass += lowers[random.randrange(0, len(lowers))]
+	
+	return xpass
+
+
+if __name__ == "__main__":
+	passwd = main(16)
+	print("Passwd:",passwd)
